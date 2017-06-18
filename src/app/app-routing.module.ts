965f7030cd18dc3f56a12fc4b68c3b694a269bd5
Justin James <digitaldrummerj@gmail.com>
2017-06-18 09:34:44 -0700

    added login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     children: []
-  }
+  },
+  { path: 'login', children: [], component: LoginComponent }
 ];
 
 @NgModule({
