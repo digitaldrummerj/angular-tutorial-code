c310d60f4a753dca8a54141ac413fbfb042cb158
Justin James <digitaldrummerj@gmail.com>
2017-06-18 10:06:58 -0700

    add login function to AuthService

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { User } from '../classes/user';
 
 @Injectable()
 export class AuthService {
+  public currentUser: User;
+  private options = new RequestOptions({ withCredentials: true });
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
 
+  login(email: string, password: string): Observable<boolean | Response> {
+    let loginInfo = { "email": email, "password": password };
+    return this.http.put("https://dj-sails-todo.azurewebsites.net/user/login", loginInfo, this.options)
+      .do((res: Response) => {
+        if (res) {
+          this.currentUser = <User>res.json();
+        }
+      })
+      .catch(error => {
+        console.log('login error', error)
+        return Observable.of(false);
+      });
+  }
 }
