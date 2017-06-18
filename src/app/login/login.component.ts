1a66ed073d0987bbd1bc6469c7b95b6cfcca8b85
Justin James <digitaldrummerj@gmail.com>
2017-06-18 10:13:21 -0700

    add call to AuthService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login(formValues) {
-    console.log(formValues);
+    this.authService.login(formValues.email, formValues.password)
+      .subscribe(result => {
+        if (!this.authService.currentUser) {
+          console.log('user not found');
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
   }
 }
