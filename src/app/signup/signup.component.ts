5e9c5b61bb21c79b28e708b37bb9dd52147de443
Justin James <digitaldrummerj@gmail.com>
2017-06-25 11:16:40 -0700

    add signup component

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  loginInvalid: boolean = false;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  signup(formValues) {
+    this.authService.signup(formValues.email, formValues.password)
+      .subscribe(result => {
+        if (!result) {
+          this.loginInvalid = true;
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
+  }
+}
