25a5de40af7303dd1b13aa04396c56ee48b2ca97
Justin James <digitaldrummerj@gmail.com>
2017-06-25 12:36:13 -0700

    working on adding dynamic validation

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
- import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -12,12 +12,45 @@ export class TodoComponent implements OnInit {
 
   ngOnInit() {
     this.addForm = this.formBuilder.group({
-       'item': ['', [Validators.required, Validators.minLength(3)]]
+      'item': ['', [Validators.required, Validators.minLength(3)]]
     });
+
+    this.addForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
+
+  onValueChanged(data?: any) {
+    if (!this.addForm) { return }
+    const form = this.addForm;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
 
   save(): void {
     console.log('form values: ', this.addForm.value);
   }
 
+  formErrors = {
+    'item': ''
+  };
+
+  validationMessages = {
+    'item': {
+      'required': 'Item is required.',
+      'minlength': 'Item must be at least 3 characters'
+    }
+  };
 }
