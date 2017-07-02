6b22b8f5ad7b8ace6743667b89566b5ec3080444
Justin James <digitaldrummerj@gmail.com>
2017-07-02 09:33:45 -0700

    added missing todo component import to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
  import { SignupComponent } from './signup/signup.component';
+import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
   {
