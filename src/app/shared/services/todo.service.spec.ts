2e7bfb4cb96c0b6b3b32303f4ac83a22b8f9f0c0
Justin James <digitaldrummerj@gmail.com>
2018-05-08 04:45:30 -0700

    remove: httpClient from test

--- src/app/shared/services/todo.service.spec.ts
+++ src/app/shared/services/todo.service.spec.ts
@@ -2,11 +2,9 @@ import { async, TestBed, inject } from '@angular/core/testing';
 import { TodoService } from './todo.service';
 import { MockTodoData, Todo } from '../../../testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('TodoService', () => {
   let service: TodoService;
-  let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
   const mockTodoData: MockTodoData = new MockTodoData();
@@ -18,7 +16,6 @@ describe('TodoService', () => {
     });
 
     service = bed.get(TodoService);
-    httpClient = TestBed.get(HttpClient);
     httpTestingController = TestBed.get(HttpTestingController);
   });
 
@@ -32,11 +29,6 @@ describe('TodoService', () => {
   });
 
   it('should get todo items', async(() => {
-    // httpClient.get<Todo[]>('/todo').subscribe(data => {
-    //   // When observable resolves, result should match test data
-    //   expect(data).toEqual( mockTodoData.todoItems);
-    //   expect(data.length).toBe(mockTodoData.todoItems.length);
-    // });
     service.getAll().subscribe(result => {
       expect(result).toEqual( mockTodoData.todoItems);
       expect(result.length).toBe(mockTodoData.todoItems.length);
