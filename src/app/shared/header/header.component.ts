dff9ad9c34fa14995a7f0dfdce597f5813062d4b
Justin James <digitaldrummerj@gmail.com>
2018-07-23 15:47:55 -0700

    fix: sync issue with cookie values set and remove

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { User } from '../classes/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit,  OnDestroy {
   isCollapsed = true;
-  loggedInUser = this.authService.getUser();
+  loggedInUser: User;
+  subscription: any;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.loggedInUser  = this.authService.getUser();
+
+    this.authService.getLoggedInUser.subscribe(user => {
+      this.loggedInUser = user;
+      console.log('user changed through emit', this.loggedInUser);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   logout() {
