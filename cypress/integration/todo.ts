7ae3dc829567163bd1a6076c9431d846e4f63592
Justin James <digitaldrummerj@gmail.com>
2019-03-04 16:54:27 -0700

    add: Todo and completion Cypress

--- cypress/integration/todo.ts
+++ cypress/integration/todo.ts
@@ -1,6 +1,6 @@
 import { recordReplayCommands } from '../support/recordReplayCommands';
 
-describe('Add Todo', () => {
+describe('Todo', () => {
   const todoText = `Todo Test Item ${Math.random()
     .toString(36)
     .substring(2, 15)}${Math.random()
@@ -16,18 +16,16 @@ describe('Add Todo', () => {
     owner: 2,
   };
 
-  beforeEach(() => {
+  before(() => {
     cy.visit('/login');
     cy.get('#email').type('foo@foo.com');
     cy.get('#password').type('123456');
     cy.get('.btn-primary')
       .should('have.text', 'Login')
       .click();
-    cy.location('pathname').should('eq', '/');
-  });
 
-  // recordReplayCommands('AddTodo', 0);
-  it.only('Add Todo', () => {
+    cy.location('pathname').should('eq', '/');
+
     cy.server();
 
     cy.route({
@@ -37,6 +35,7 @@ describe('Add Todo', () => {
       response: addResponse,
     }).as('addtodo');
 
+
     cy.get('.form-control').type(todoText);
 
     cy.get('.btn')
@@ -44,47 +43,84 @@ describe('Add Todo', () => {
       .click();
 
     cy.wait('@addtodo');
-
-    const firstRow = cy.get('.row.todo').first();
-    firstRow.find('[data-cy=todo-text]').should('contain', todoText);
-
-    cy.get('[data-cy="check-icon"] svg[data-icon="check-square"]')
-      .and('not.be.visible');
-
-      cy.get('[data-cy="trash-icon"] svg[data-icon="trash-alt"]')
-      .and('be.visible');
-
-    cy.get('.lead').should('contain', "You've got 1 things to do");
   });
 
-  it('MinLength Validation', () => {
-    cy.get('.form-control')
-      .type('1')
-      .get('.alert')
-      .should('contain', 'Item must be at least 3 characters');
+  describe('Complete Todo', () => {
+    it('Complete Item', () => {
+      const firstRow = cy.get('.row.todo').first();
 
-    cy.get('.form-control')
-      .type('123')
-      .get('.alert')
-      .should('not.be.visible');
+      cy.route({
+        method: 'PATCH',
+        url: '/todo/*',
+        response: 'OK',
+      }).as('todo-complete');
+
+      // Toggle to Completed
+      cy.get('[data-cy="check-icon"] svg[data-icon="square"]')
+        .and('be.visible')
+        .click();
+
+      cy.wait('@todo-complete');
+
+      cy.get('[data-cy="check-icon"] svg[data-icon="check-square"]').and('be.visible');
+
+      cy.get('[data-cy="check-icon"] svg[data-icon="square"]').and('not.be.visible');
+
+      // Toggle to Uncompleted
+      cy.get('[data-cy="check-icon"] svg[data-icon="check-square"]').click();
+
+      cy.wait('@todo-complete');
+
+      cy.get('[data-cy="check-icon"] svg[data-icon="square"]').and('be.visible');
+
+      cy.get('[data-cy="check-icon"] svg[data-icon="check-square"]').and('not.be.visible');
+    });
   });
 
-  it('Required Validation', () => {
-    cy.get('.form-control')
-      .type('1')
-      .clear()
-      .get('.alert')
-      .should('contain', 'Item is required.');
+  describe('Add Todo', () => {
+    // recordReplayCommands('AddTodo', 0);
+    it('Add', () => {
+      const firstRow = cy.get('.row.todo').first();
+      firstRow.find('[data-cy=todo-text]').should('contain', todoText);
 
-    cy.get('.form-control')
-      .type('1')
-      .get('.alert')
-      .should('contain', 'Item must be at least 3 characters');
+      cy.get('[data-cy="check-icon"] svg[data-icon="square"]').and('be.visible');
 
-    cy.get('.form-control')
-      .clear()
-      .type('123')
-      .get('alert')
-      .should('not.be.visible');
+      cy.get('[data-cy="check-icon"] svg[data-icon="check-square"]').and('not.be.visible');
+
+      cy.get('[data-cy="trash-icon"] svg[data-icon="trash-alt"]').and('be.visible');
+
+      cy.get('.lead').should('contain', "You've got 1 things to do");
+    });
+
+    it('MinLength Validation', () => {
+      cy.get('.form-control')
+        .type('1')
+        .get('.alert')
+        .should('contain', 'Item must be at least 3 characters');
+
+      cy.get('.form-control')
+        .type('123')
+        .get('.alert')
+        .should('not.be.visible');
+    });
+
+    it('Required Validation', () => {
+      cy.get('.form-control')
+        .type('1')
+        .clear()
+        .get('.alert')
+        .should('contain', 'Item is required.');
+
+      cy.get('.form-control')
+        .type('1')
+        .get('.alert')
+        .should('contain', 'Item must be at least 3 characters');
+
+      cy.get('.form-control')
+        .clear()
+        .type('123')
+        .get('alert')
+        .should('not.be.visible');
+    });
   });
 });
