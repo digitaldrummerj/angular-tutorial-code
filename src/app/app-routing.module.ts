b0263fd8891a865f34110d1e12195605af2c4226
Justin James <digitaldrummerj@gmail.com>
2019-07-10 04:33:54 -0700

    fix: mis-aligned imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
- import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', children: [], component: LoginComponent },
