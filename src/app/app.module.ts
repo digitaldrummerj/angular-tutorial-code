cac8f3d837feb95066944d7fabbb486f0509899a
Justin James <digitaldrummerj@gmail.com>
2019-07-10 04:22:45 -0700

    update: ngx-bootstrap to v5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
+    CollapseModule.forRoot(),
+    BrowserAnimationsModule,
     AppRoutingModule,
   ],
   providers: [],
